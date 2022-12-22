49024d1f071119238fab630c8e9ddece4f03a4cb
lucas <lucas.dantas.nogueira@gmail.com>
2022-12-22 11:46:16 -0300

    Adicionando rotas para pegar os likes do post e inserir dados dos comentário no bd do comentário do post

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,19 @@
 
 Broadcast::routes();
 
+Route::post('/comment', 'Comment\CommentController@store')->name('comment.store');
+
+Route::put('/load-likes', 'Likes\LikeController@loadLikes')->name('like.load');
+
+Route::put('/likes', 'Likes\LikeController@likes')->name('like');
+
 Route::put('notification-all-read', 'NotificationController@markAllAsRead')->name('notification.all.read');
 
 Route::put('notification-read', 'NotificationController@markAsRead')->name('notification.read');
 
-Route::get('notifications', 'NotificationController@notifications')->name('notifications');
+Route::get('post/delete/{id}', 'Posts\PostController@delete')->name('post.delete');
 
-Route::get('post/like/{id}', 'Likes\LikeController@create')->name('like.create');
+Route::get('notifications', 'NotificationController@notifications')->name('notifications');
 
 Route::resource('post', 'Posts\PostController');
 
